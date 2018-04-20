f5334ad32d4faa3302504d6ee64f5d3827b16453
Zhuyunfeng <i@zhuyunfeng.com>
2018-04-20 17:52:30 +0800

    rename

---
+++ app/js/class/1let&const.js
@@ -0,0 +1,16 @@
+function test() {
+  // let a=21;
+  // for(let i=1;i<3;i++){
+  //   console.log(i);
+  // }
+  // console.log(i);
+  let a=1;
+  const PI=3.141592653;
+  // PI=8;
+  const k={
+    a:1
+  }
+  k.b=3;
+  console.log(PI,k);
+}
+test();
